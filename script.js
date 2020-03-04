e4ce8839280f0f4654941556b985471bc5f29e8b
TimzOwen <ktimothy@kabarak.ac.ke>
2020-03-04 14:56:33 +0300

    javascript opeator handler

--- script.js
+++ script.js
@@ -8,4 +8,35 @@ function getHistory() {
 function printHistory(num) {
     document.getElementById("history-value").innerText = num;
 }
-printHistory("9*9+8");
+//function to get output
+function getOutPut() {
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num) {
+    if (num == "") {
+        document.getElementById("output-value").innerText = num;
+    } else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+
+//function to separate the value in threes
+function getFormattedNumber(num) {
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+//function to reverse the number format 
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ''));
+}
+// alert(reverseNumberFormat(getOutPut()));
+
+//work on the operators
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function() {
+        alert("the operator clicked s " + this.id);
+    })
+}
